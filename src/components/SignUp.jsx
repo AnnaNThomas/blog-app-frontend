914c77f970ddbc684f223315d4a88edcf26fada5
AnnaNThomas <annathomas2003ned@gmail.com>
2024-06-13 14:31:32 +0530

    signUp frontend completed

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row g-3">
+                    <h1>SIGN Up</h1>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">password</label>
+                    <input type="text" className="form-control" />
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-6">
+                        <button className="btn btn-primary">Submit</button>
+                    </div>
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <p><a href="#" class="link-primary">click here to go back</a></p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default SignUp
